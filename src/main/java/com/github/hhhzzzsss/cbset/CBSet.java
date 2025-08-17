6cac84d5a1f285b38bc21846947e40484b736d06
hhhzzzsss <hyzhou42@gmail.com>
2025-08-16 23:06:33 -0500

    Add csetbook command

--- src/main/java/com/github/hhhzzzsss/cbset/CBSet.java
+++ src/main/java/com/github/hhhzzzsss/cbset/CBSet.java
@@ -1,6 +1,7 @@
 package com.github.hhhzzzsss.cbset;
 
 import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ModInitializer;
@@ -15,11 +16,16 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.contents.TranslatableContents;
+import net.minecraft.network.protocol.game.ServerboundEditBookPacket;
 import net.minecraft.network.protocol.game.ServerboundSetCommandBlockPacket;
 import net.minecraft.server.dialog.Dialog;
 import net.minecraft.world.level.block.entity.CommandBlockEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class CBSet implements ModInitializer {
 	public static final String MOD_ID = "cbset";
@@ -31,6 +37,7 @@ public class CBSet implements ModInitializer {
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
 			dispatcher.register(getCBSetCommand());
 			dispatcher.register(getCDialogCommand(registryAccess));
+			dispatcher.register((getCSetBookCommand()));
 		});
 
 		ClientReceiveMessageEvents.ALLOW_GAME.register((component, overlay) -> filterMessage(component));
@@ -62,6 +69,20 @@ public class CBSet implements ModInitializer {
 		);
 	}
 
+	private LiteralArgumentBuilder<FabricClientCommandSource> getCSetBookCommand() {
+		return ClientCommandManager.literal("csetbook").then(
+				ClientCommandManager.argument("slot", IntegerArgumentType.integer(0, 8)).then(
+						ClientCommandManager.argument("data", StringArgumentType.greedyString()).executes(ctx -> {
+							setBook(
+									ctx.getArgument("slot", Integer.class),
+									ctx.getArgument("data", String.class)
+							);
+							return 1;
+						})
+				)
+		);
+	}
+
 	private LiteralArgumentBuilder<FabricClientCommandSource> getCDialogCommand(CommandBuildContext registryAccess) {
 		return ClientCommandManager.literal("cdialog").then(
 			ClientCommandManager.argument("dialog", ResourceOrIdArgument.dialog(registryAccess)).executes( ctx -> {
@@ -78,14 +99,59 @@ public class CBSet implements ModInitializer {
 		);
 	}
 
-	private boolean filterMessage(Component component) {
-		if (component.getContents() instanceof TranslatableContents contents) {
-            return !contents.getKey().equals("advMode.setCommand.success");
+	private void setBook(int slot, String text) {
+		text = escapeBookText(text);
+		List<String> pages = Arrays.asList(text.split("\f"));
+		Minecraft.getInstance().getConnection().send(
+				new ServerboundEditBookPacket(slot, pages, Optional.empty())
+		);
+
+	}
+
+	private String escapeBookText(String raw) {
+		StringBuilder sb = new StringBuilder();
+		boolean escape = false;
+
+		for (int i = 0; i < raw.length(); i++) {
+			char c = raw.charAt(i);
+			if (escape) {
+				switch (c) {
+					case '&': sb.append('&'); break;
+					case 'n': sb.append('\n'); break;
+					case 'f': sb.append('\f'); break;
+					case '\\': sb.append('\\'); break;
+					default:
+						sb.append('\\').append(c);
+						break;
+				}
+				escape = false;
+			} else {
+				if (c == '\\') {
+					escape = true;
+				} else if (c == '&') {
+					sb.append('§');
+				} else {
+					sb.append(c);
+				}
+			}
 		}
-		return true;
+
+		// Handle trailing backslash
+		if (escape) {
+			sb.append('\\');
+		}
+
+		return sb.toString();
 	}
 
 	private void sendDialog(Holder<Dialog> dialog) {
 		Minecraft.getInstance().schedule(() -> Minecraft.getInstance().getConnection().showDialog(dialog, Minecraft.getInstance().screen));
+	}
+
+	private boolean filterMessage(Component component) {
+		if (component.getContents() instanceof TranslatableContents contents) {
+			return !contents.getKey().equals("advMode.setCommand.success");
+		}
+		return true;
 	}
 }
